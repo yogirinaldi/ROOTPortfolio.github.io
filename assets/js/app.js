354ba3472d310e3858932f982b58847a1cc56701
Jannen Wollyn Survino Siahaan <92134528+jannenwollyn@users.noreply.github.com>
2023-01-13 12:42:20 +0700

    Add New Menu Contact
    In Navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="contact.html" target="_blank">Contact</a></li>
    <li class="nav-item">
    
    <div class="bike">
